0e9aa3b382902d0f11e16d584305345705231db8
Miles Collins <miles.collins@ymail.com>
2022-08-08 16:51:24 -0600

    Fake Database update

--- server/db/FakeDb.js
+++ server/db/FakeDb.js
@@ -1,5 +1,11 @@
 export const Burgers_DB = {
   burgers: [
     { id: 0, name: 'The Miles Collins', Meat: 'Chicken Breast', Condiments: 'pickles', Sauce: 'Special Sauce'}
+    { id: 1, name: 'The Jon McNamara', Meat: 'Turkey Patty', Condiments: 'lettuce, tomato, jalapeno', Sauce: 'Special Sauce'}
+    { id: 2, name: 'The Chris Withers', Meat: '1/2 lb Beef Patty, bacon', Condiments: 'Onion Rings, Pickles, ', Sauce: 'A1 Sauce'}
+    { id: 3, name: 'The Darion Keeton', Meat: '2 Beef Patties, Bacon', Condiments: 'pickles', Sauce: 'Special Sauce'}
+    { id: 4, name: 'The Cameron Perez', Meat: 'Pulled Pork', Condiments: 'pickles', Sauce: 'Special Sauce'}
+    { id: 5, name: 'The Mike Ocampo', Meat: 'SPAM Patty', Condiments: 'Onions, Lettuce, Tomato, Cucumbers', Sauce: 'Ketchup, Mustard'}
+    { id: 5, name: 'The Alysha Sibulkin', Meat: 'Bison', Condiments: 'pickles', Sauce: 'BBQ Sauce'}
   ]
 }
